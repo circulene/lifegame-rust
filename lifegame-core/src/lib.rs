c2f23e2b1ce09d58e747f880ae87447735f09022
circulene <circulene@gmail.com>
2024-07-16 19:26:19 +0900

    add torus bound

--- lifegame-core/src/lib.rs
+++ lifegame-core/src/lib.rs
@@ -1,15 +1,33 @@
 use anyhow::{Error, Result};
 use bit_vec::BitVec;
+use WorldBound::{Plane, Torus};
 
 pub type Cell = bool;
 pub const CELL_DEAD: bool = false;
 pub const CELL_ALIVE: bool = true;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum WorldBound {
+    Plane,
+    Torus,
+}
+
+fn get_index_with_cyclic_bound(ix: isize, bound: isize) -> isize {
+    if ix < 0 {
+        ((ix + bound) % bound).abs()
+    } else if ix >= bound {
+        ix % bound
+    } else {
+        ix
+    }
+}
 
 #[derive(Debug)]
 pub struct World {
     nx: usize,
     ny: usize,
     cells: BitVec,
+    border: WorldBound,
 }
 
 impl World {
@@ -22,7 +40,12 @@ impl World {
             nx,
             ny,
             cells: to_bitvec(&cells),
+            border: Plane,
         })
+    }
+
+    pub fn border(&mut self, border: WorldBound) {
+        self.border = border;
     }
 
     fn bit_index(&self, ix: usize, iy: usize) -> usize {
@@ -67,33 +90,46 @@ impl World {
         self.cells = next_cells;
     }
 
-    fn neighbours(&self, ix: usize, iy: usize) -> BitVec {
-        let get_cell = |ix, iy| {
-            if ix < 0 || iy < 0 || ix as usize >= self.nx || iy as usize >= self.ny {
-                CELL_DEAD
-            } else {
-                self.cell(ix as usize, iy as usize)
+    /// get cell with border consideration
+    fn get_cell_with_border(&self, ix: isize, iy: isize) -> bool {
+        let (nx, ny) = (self.nx as isize, self.ny as isize);
+        match self.border {
+            Plane => {
+                if ix < 0 || iy < 0 || ix >= nx || iy >= ny {
+                    CELL_DEAD
+                } else {
+                    self.cell(ix as usize, iy as usize)
+                }
             }
-        };
+            Torus => self.cell(
+                get_index_with_cyclic_bound(ix, nx) as usize,
+                get_index_with_cyclic_bound(iy, ny) as usize,
+            ),
+        }
+    }
+
+    /// get bitmap representing Moore neighbours in following order.
+    /// [MSB] SE S SW E W NE N NW [LSB]
+    fn neighbours(&self, ix: usize, iy: usize) -> BitVec {
         let (ix, iy) = (ix as isize, iy as isize);
         let bitmap: BitVec = to_bitvec(&[
-            get_cell(ix - 1, iy - 1),
-            get_cell(ix - 1, iy),
-            get_cell(ix - 1, iy + 1),
-            get_cell(ix, iy - 1),
-            get_cell(ix, iy + 1),
-            get_cell(ix + 1, iy - 1),
-            get_cell(ix + 1, iy),
-            get_cell(ix + 1, iy + 1),
+            self.get_cell_with_border(ix - 1, iy - 1), // NW
+            self.get_cell_with_border(ix, iy - 1),     // N
+            self.get_cell_with_border(ix + 1, iy - 1), // NE
+            self.get_cell_with_border(ix - 1, iy),     // W
+            self.get_cell_with_border(ix + 1, iy),     // E
+            self.get_cell_with_border(ix - 1, iy + 1), // SW
+            self.get_cell_with_border(ix, iy + 1),     // S
+            self.get_cell_with_border(ix + 1, iy + 1), // SE
         ]);
         bitmap
     }
 }
 
 fn to_bitvec(bits: &[bool]) -> BitVec {
-    let mut bitvec = BitVec::from_elem(bits.len(), false);
-    for (i, bit) in bits.iter().enumerate() {
-        bitvec.set(i, *bit);
+    let mut bitvec = BitVec::with_capacity(bits.len());
+    for bit in bits.iter() {
+        bitvec.push(*bit);
     }
     bitvec
 }
@@ -116,6 +152,46 @@ mod tests {
             &space.unwrap().cells,
             &to_bitvec(&[CELL_ALIVE, CELL_DEAD, CELL_DEAD, CELL_DEAD])
         );
+    }
+
+    #[test]
+    fn test_cyclic_bound() {
+        assert_eq!(get_index_with_cyclic_bound(-21, 10), 1);
+        assert_eq!(get_index_with_cyclic_bound(-1, 10), 9);
+        assert_eq!(get_index_with_cyclic_bound(0, 10), 0);
+        assert_eq!(get_index_with_cyclic_bound(9, 10), 9);
+        assert_eq!(get_index_with_cyclic_bound(10, 10), 0);
+        assert_eq!(get_index_with_cyclic_bound(21, 10), 1);
+    }
+
+    #[test]
+    fn test_neighbour() -> Result<()> {
+        // plane
+        let world = World::new(
+            2,
+            2,
+            [[CELL_ALIVE, CELL_ALIVE], [CELL_ALIVE, CELL_ALIVE]].concat(),
+        )?;
+        assert_eq!(world.get_cell_with_border(-1, -1), CELL_DEAD);
+        assert_eq!(world.get_cell_with_border(-1, 0), CELL_DEAD);
+        assert_eq!(world.get_cell_with_border(0, 0), CELL_ALIVE);
+        assert_eq!(world.get_cell_with_border(1, 1), CELL_ALIVE);
+        assert_eq!(world.get_cell_with_border(2, 2), CELL_DEAD);
+
+        // torus
+        let mut world = World::new(
+            2,
+            2,
+            [[CELL_ALIVE, CELL_DEAD], [CELL_DEAD, CELL_ALIVE]].concat(),
+        )?;
+        world.border(Torus);
+        assert_eq!(world.get_cell_with_border(-1, -1), CELL_ALIVE);
+        assert_eq!(world.get_cell_with_border(-1, 0), CELL_DEAD);
+        assert_eq!(world.get_cell_with_border(0, 0), CELL_ALIVE);
+        assert_eq!(world.get_cell_with_border(1, 1), CELL_ALIVE);
+        assert_eq!(world.get_cell_with_border(2, 2), CELL_ALIVE);
+
+        Ok(())
     }
 
     #[test]
